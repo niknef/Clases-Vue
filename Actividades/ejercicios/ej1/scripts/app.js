2800880180a501b6d793e4b7188fc81d45082b53
NikNef <nicolas.firpo@davinci.edu.ar>
2024-06-22 19:21:18 -0300

    add ej 1, 2

---
+++ Actividades/ejercicios/ej1/scripts/app.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '.contenedor',
+    data: {
+        title: 'Super Mario 3d World + Bowser´s Fury',
+        imagen_mario : 'img/mario.jpg',
+        alt: 'Portada del juego de Super Mario 3d Word',
+        h2_mario:'<h2>Salvá el Reino de las hadas!</h2>',
+        h2_bowser: 'Explora un mundo felino con Bowser’s Fury',
+        datazo: false
+    }
+});
